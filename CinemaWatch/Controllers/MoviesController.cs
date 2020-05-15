603cffb33c5b7ede50fe3aa1f6791e63eda16964
Brett Fishel <brett.fishel@oyova.com>
2020-05-15 18:38:11 -0400

    Add project files.

---
+++ CinemaWatch/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using CinemaWatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CinemaWatch.ViewModels;
+
+namespace CinemaWatch.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "The Thing" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Elton" },
+                new Customer { Name = "John"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year:regex(\\d{2})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+    }
+}
