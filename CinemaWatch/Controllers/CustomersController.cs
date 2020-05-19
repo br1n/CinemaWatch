78342b8c7bd76d1d0e5fda327cf7e71fe047ce42
Brett Fishel <brett.fishel@oyova.com>
2020-05-19 12:33:11 -0400

    Load customers from database

--- CinemaWatch/Controllers/CustomersController.cs
+++ CinemaWatch/Controllers/CustomersController.cs
@@ -1,26 +1,40 @@
-﻿using CinemaWatch.Models;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Entity;
+using CinemaWatch.Models;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace CinemaWatch.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if(customer == null)
             {
@@ -30,15 +44,5 @@ namespace CinemaWatch.Controllers
             return View(customer);
         }
 
-
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith"},
-                new Customer { Id = 2, Name = "Devon Goetz"}
-            };
-        }
     }
 }
