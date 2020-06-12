eeb7d40edee3e462f6b0593292542c60bbf12734
Brett Fishel <brett.fishel@oyova.com>
2020-06-12 16:59:34 -0400

    Add NumberAvailable save to NumberInStock for new movies

--- CinemaWatch/Controllers/MoviesController.cs
+++ CinemaWatch/Controllers/MoviesController.cs
@@ -93,6 +93,7 @@ namespace CinemaWatch.Controllers
             if(movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
+                movie.NumberAvailable = movie.NumberInStock;
                 _context.Movies.Add(movie);
             }
             else
